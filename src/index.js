6356a41cdc8f40a76c453415c419b836d27312e9
Jesus <codeconqueror777@gmail.com>
2024-03-16 15:26:27 -0400

    Guardando el proyecto a github, etc

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './styles.css';
-import { Todo } from './classes/todo.class';
+import { Todo, TodoList } from "./classes/index";
+import { crearTodoHtml } from './js/componentes';
 
-const tarea = new Todo('Aprender JavaScript');
+export const todoList = new TodoList();
+
+todoList.todos.forEach( crearTodoHtml );
+
+console.log('todos', todoList.todos);
